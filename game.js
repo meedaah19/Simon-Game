25094bec617ce4f2b17558706d502434395a9800
Hameedat Oyewopo Timileyin <161971114+meedaah19@users.noreply.github.com>
2024-06-21 15:51:22 +0100

    Update game.js

--- game.js
+++ game.js
@@ -47,7 +47,8 @@ function checkAnswer(currentLevel) {
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
-      
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
       }
 }
 
